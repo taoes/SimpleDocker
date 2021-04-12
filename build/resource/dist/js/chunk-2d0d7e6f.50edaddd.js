44febb6220869f04e1dd083d938529199117bbe6
周涛 <zhoutao825638@vip.qq.com>
2021-04-12 14:43:58 +0800

    :sparkles: 发布0.0.5 版本

---
+++ build/resource/dist/js/chunk-2d0d7e6f.50edaddd.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d7e6f"],{7990:function(e,t,n){"use strict";n.r(t);var c=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"secret"},[n("h1",[e._v("安全认证功能暂未完成")])])}],r=n("2877"),l={},u=Object(r["a"])(l,c,s,!1,null,null,null);t["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d0d7e6f.50edaddd.js.map
--- script/linux-deploy.sh
+++
@@ -1,9 +0,0 @@
-echo "正在准备安装 SimpleDocker...."
-
-wget 'https://gitee.com/taoes_admin/SimpleDocker/attach_files/587034/download/SimpleDocker-0.0.3-linux.zip' -O '/tmp/simpleDocker.zip'
-rm -rf ~/.local/simpleDocker
-mkdir -p ~/.local/simpleDocker
-unzip /tmp/simpleDocker.zip -d ~/.local/simpleDocker
-echo "export PATH=\$PATH:~/.local/simpleDocker" >> ~/.bashrc
-source ~/.bashrc
-echo "安装完成,请使用命令 SimpleDocker 启动后，在浏览器中访问 4050 端口...."
