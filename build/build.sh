955b584f0af0aa763d72a182e81f66f194b578ab
yan <zhoutao825638@vip.qq.com>
2022-06-23 23:57:14 +0800

    :zap: add build script for github workflow

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,14 @@
-cd ./front-src
-echo "编译前端.......START!"
-yarn build:prod
-echo "编译前端资源文件.......OK!"
+function print_info() {
+    echo $0
+}
+
+cd ./admin
+print_info "编译前端.......START!"
+npm install yarn  && yarn install && yarn build:prod
+print_info "编译前端资源文件.......OK!"
 
 # shellcheck disable=SC2103
-cd ..
+cd ./backend-src
 rm -rf ./backend-src/src/main/resources/static
 mkdir -p ./backend-src/src/main/resources/static
 cp -rf ./front-src/build/ ./backend-src/src/main/resources/static/
