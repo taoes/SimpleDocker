08055747a5e15448cb5c62d8bf9014d280039a6e
yan <zhoutao825638@vip.qq.com>
2022-07-10 18:37:12 +0800

    :zap: 导出SQL & DATA

--- database/struct_data.sql
+++ database/struct_data.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80027
  File Encoding         : 65001
 
- Date: 10/07/2022 18:34:31
+ Date: 10/07/2022 18:35:22
 */
 
 SET NAMES utf8mb4;
@@ -35,6 +35,14 @@ CREATE TABLE `docker_endpoint` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='dockerEndpoint';
 
 -- ----------------------------
+-- Records of docker_endpoint
+-- ----------------------------
+BEGIN;
+INSERT INTO `docker_endpoint` (`id`, `name`, `type`, `state`, `host`, `port`, `latest_test_time`, `created_at`, `updated_at`) VALUES ('DEFAULT', '本地Docker', 'LOCAL', 'NORMAL', '', 0, '2022-06-14 13:34:18', '2022-06-14 12:28:17', '2022-06-14 12:28:17');
+INSERT INTO `docker_endpoint` (`id`, `name`, `type`, `state`, `host`, `port`, `latest_test_time`, `created_at`, `updated_at`) VALUES ('DEFAULT2', '远程Docker', 'REMOTE', 'NORMAL', '192.168.1.104', 8082, '2022-06-14 13:34:19', '2022-06-14 12:29:23', '2022-06-14 12:29:23');
+COMMIT;
+
+-- ----------------------------
 -- Table structure for record
 -- ----------------------------
 DROP TABLE IF EXISTS `record`;
@@ -47,6 +55,12 @@ CREATE TABLE `record` (
   `content` text COMMENT '操作内容JSON',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='操作记录表';
+
+-- ----------------------------
+-- Records of record
+-- ----------------------------
+BEGIN;
+COMMIT;
 
 -- ----------------------------
 -- Table structure for system_config
@@ -62,6 +76,12 @@ CREATE TABLE `system_config` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
+-- Records of system_config
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
@@ -75,5 +95,13 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+BEGIN;
+INSERT INTO `user` (`id`, `username`, `password`, `salt_value`, `created_at`, `updated_at`) VALUES (1, 'admin', '123456', '789', '2022-06-12 01:49:22', '2022-06-12 01:49:22');
+INSERT INTO `user` (`id`, `username`, `password`, `salt_value`, `created_at`, `updated_at`) VALUES (2, 'tao', '123321', '12321312', '2022-06-13 00:05:25', '2022-06-13 00:05:25');
+COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
